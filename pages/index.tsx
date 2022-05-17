e3c44ae8f5ad27d324025c2355ceedfd995fa616
ton1czech <tonyasek007@gmail.com>
2022-05-17 22:23:29 +0200

    🎁 NEW: template

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+      <Head>
+        <title>template</title>
+        <link rel='icon' href='' />
+      </Head>
+    </div>
+  )
+}
+
+export default Home
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+}
